fc0a59380cadbde3e03a31cd75aaaddb03cdbb97
Tonia Saba <teesabz@Tonias-MacBook-Pro.local>
2020-04-18 22:43:12 -0400

    created game logic

--- memory_gamefinal/js/main.js
+++ memory_gamefinal/js/main.js
@@ -4,5 +4,21 @@ let cardOne = "queen";
 let cardTwo = "queen";
 let cardThree = "king";
 let cardFour = "king";
+
+
+const cards = ["queen", "queen", "king", "king"];
+cardInPlay = []; 
+
+cardOne = cards[0];
+cardTwo = cards[2];
+cardInPlay.push(cardOne); 
+cardInPlay.push(cardTwo); 
+
 console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+console.log("User flipped " + cardTwo);
+
+if (cardInPlay[0] === cardInPlay[1]) {
+  alert("You found a match!");
+} else {
+  alert("Sorry, try again.");
+}
